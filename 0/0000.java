5998135f023fef5f3dfb82f93be4e6a79954a8cb
Yuki Shiraishi <yksris94@gmail.com>
2016-09-15 01:12:29 +0900

    Create 0000.java

---
+++ 0/0000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+class Main{
+     
+    public static void main(String args[]){
+        for(int times = 1; times <= 9; times++){
+            for(int n = 1; n <= 9; n++){
+                System.out.println(times + "x" + n + "=" + times * n);
+            }
+        }
+    }
+}
